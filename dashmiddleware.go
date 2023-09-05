11c3870cb92c705c96021e6b1f70d5dc2e6803fe
VK <viktor@krueckl.de>
2023-09-05 16:55:03 +0200

    only track _dash-update-component calls

--- dashmiddleware.go
+++ dashmiddleware.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // Config the plugin configuration.
@@ -57,53 +58,61 @@ func (c *DashMiddleware) ServeHTTP(responseWriter http.ResponseWriter, req *http
 	_, cancel := context.WithTimeout(ctx, 10)
 	defer cancel()
 
+	// Read the request body
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Printf("Failed to read request body: %v", err)
+		return
+	}
+	// Restore the original request body for downstream handlers
+	req.Body = io.NopCloser(bytes.NewBuffer(body))
+
 	// Create a capturing response writer
 	capturingWriter := &CapturingResponseWriter{
 		ResponseWriter: responseWriter, Body: []byte{},
 	}
 
 	// Continue the request down the middleware chain
-	// c.next.ServeHTTP(responseWriter, req)
 	c.next.ServeHTTP(capturingWriter, req)
 
 	url := req.URL.String()
 
-	body, err := io.ReadAll(req.Body)
-	if err != nil {
-		log.Printf("Failed to read request body: %v", err)
-		return
-	}
+	// Check if the URL ends with "/_dash-update-component"
+	if strings.HasSuffix(url, "/_dash-update-component") {
+		// Remove "/_dash-update-component" from the URL
+		url = strings.TrimSuffix(url, "/_dash-update-component")
 
-	// Define the JSON payload to send in the request body
-	payload := map[string]interface{}{
-		"Request": string(body),
-		"Result":  string(capturingWriter.Body),
-		"URL":     url,
-		"User":    "SomeUserValue",
-	}
-
-	// Marshal the payload into a JSON string
-	payloadJSON, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Failed to create JSON payload: %v", err)
-		return
-	}
-
-	// Make a request to the external REST API to track the request
-	resp, err := http.Post(c.trackURL, "application/json", bytes.NewBuffer(payloadJSON))
-	if err != nil {
-		log.Printf("Failed to track request: %v", err)
-		return
-	}
-	defer func() {
-		if closeErr := resp.Body.Close(); closeErr != nil {
-			log.Printf("Error closing response body: %v", closeErr)
+		// Define the JSON payload to send in the request body
+		payload := map[string]interface{}{
+			"Request": string(body),
+			"Result":  string(capturingWriter.Body),
+			"URL":     url,
+			"User":    "SomeUserValue",
 		}
-	}()
 
-	// Check the response status code from the external API
-	if resp.StatusCode != http.StatusOK {
-		log.Printf("Failed to track request. Status Code: %d", resp.StatusCode)
-		return
+		// Marshal the payload into a JSON string
+		payloadJSON, err := json.Marshal(payload)
+		if err != nil {
+			log.Printf("Failed to create JSON payload: %v", err)
+			return
+		}
+
+		// Make a request to the external REST API to track the request
+		resp, err := http.Post(c.trackURL, "application/json", bytes.NewBuffer(payloadJSON))
+		if err != nil {
+			log.Printf("Failed to track request: %v", err)
+			return
+		}
+		defer func() {
+			if closeErr := resp.Body.Close(); closeErr != nil {
+				log.Printf("Error closing response body: %v", closeErr)
+			}
+		}()
+
+		// Check the response status code from the external API
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("Failed to track request. Status Code: %d", resp.StatusCode)
+			return
+		}
 	}
 }
